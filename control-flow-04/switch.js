32bfc820a6d4f6635610c7ab7757f7d527e0c165
neerajgir <neerajgoswami871@gmail.com>
2025-03-10 02:28:23 +0500

    add switch.js to demonstrate the use of switch case statements in JavaScript

---
+++ control-flow-04/switch.js
@@ -0,0 +1,31 @@
+//switch case - in this case we are using switch case to check the value of the variable and based on the value we are returning the value
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log('january');
+        break;
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    case 4:
+        console.log('April');
+        break;
+
+    default:
+        console.log('default');
+        break;
+}
